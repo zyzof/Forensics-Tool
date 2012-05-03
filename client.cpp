0e7b32349f6df9eb5e26dd0e0b7b555fa42633be
Joshua Scott <joppyfurr@gmail.com>
2012-05-03 21:14:54 +1200

    Server thingies - It lets you rstart the server quick

--- client.cpp
+++ client.cpp
@@ -25,6 +25,10 @@ void *threaded_text(void *p) {
         pthread_mutex_unlock(&read_mutex);
         buffer[n] = '\0';
         if(n) {
+            if(!strncmp(buffer, "__Exit__", 8)) { /* Server halted */
+                *(int *)p = -1;
+                break;
+            }
             printf("%s", buffer);
             fflush(stdout);
         }
@@ -103,6 +107,15 @@ void remote_access(char *ip) {
             n = 0;
         }
         out_buffer[n] = '\0';
+
+        if(!strncmp(out_buffer, "__Exit__", 8)) {
+            the_socket = -1;
+        }
+        if(the_socket == -1) {
+            connected = 0;
+            break;
+        }
+
         while(n > 0) {
             printf(out_buffer);
             pthread_mutex_lock(&read_mutex);
@@ -112,6 +125,8 @@ void remote_access(char *ip) {
         }
         
     }
+
+    printf("Connection closed.\n");
     
     pthread_mutex_destroy(&read_mutex);
 }
